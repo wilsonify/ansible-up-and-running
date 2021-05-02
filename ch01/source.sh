dabecd410f00d1a705d1e0c849677a94d239d56b
Bas Meijer <bas.meijer@me.com>
2021-05-02 12:34:33 +0200

    run-from-source

--- ch01/source.sh
+++
@@ -1,9 +0,0 @@
-# source this file to run ansible out of checkout
-python3 -m venv .venv --prompt S
-source .venv/bin/activate
-python3 -m pip install --upgrade pip
-pip3 install wheel
-git clone https://github.com/ansible/ansible.git --recursive
-pip3 install -r ansible/requirements.txt
-cd ./ansible
-source ./hacking/env-setup
