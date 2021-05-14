877a5d0bf0344ea7eb6536b23ce6dee220447cbe
Bas Meijer <bas.meijer@me.com>
2021-05-14 16:57:33 +0200

    updated for python3

--- ch05/mezzanine-dev.sh
+++ ch05/mezzanine-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-virtualenv venv
+python3 -m venv venv
 source venv/bin/activate
-pip install mezzanine
+pip3 install mezzanine
 mezzanine-project myproject
 cd myproject
 sed -i.bak 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["127.0.0.1"]/' myproject/settings.py
-python manage.py createdb
-python manage.py runserver
+python3 manage.py createdb
+python3 manage.py runserver
