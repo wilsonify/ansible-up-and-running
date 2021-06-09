10db4e3ca5fa9c43408090534cce8fcd4c620cff
Bas Meijer <bas.meijer@me.com>
2021-06-09 17:26:04 +0200

    shellchecked

--- ch05/mezzanine-dev.sh
+++ ch05/mezzanine-dev.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 python3 -m venv venv
+# shellcheck disable=SC1091
 source venv/bin/activate
 pip3 install mezzanine
 mezzanine-project myproject
-cd myproject
+cd myproject || exit
 sed -i.bak 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = ["127.0.0.1"]/' myproject/settings.py
 python3 manage.py createdb
 python3 manage.py runserver
